fc0c3f075310ce22ccb536a411f07ff15110a11e
NguyenHuuHung <huuhung.9a10.2013@gmail.com>
2026-01-27 23:42:54 +0700

    setup redis in infrastructure

--- services/ShoppeeClone.Infrastructure/DependencyInjection.cs
+++ services/ShoppeeClone.Infrastructure/DependencyInjection.cs
@@ -10,23 +10,41 @@ using ShoppeeClone.Infrastructure.Authentication.Password;
 using ShoppeeClone.Application.Common.Interfaces.Authentication.Password;
 using ShoppeeClone.Application.Common.Interfaces.Authentication;
 using ShoppeeClone.Infrastructure.Authentication.RefreshTokens;
+using StackExchange.Redis;
+using Microsoft.Extensions.Options;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection infrastructures, IConfiguration configuration)
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection infrastructures,
+        IConfiguration configuration)
     {
-        infrastructures.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+        infrastructures.Configure<JwtSettings>(
+            configuration.GetSection("JwtSettings"));
+
+        infrastructures.Configure<RedisSettings>(
+            configuration.GetSection("Redis"));
+
+        infrastructures.AddSingleton<IConnectionMultiplexer>(sp =>
+        {
+            var redisSettings = sp
+                .GetRequiredService<IOptions<RedisSettings>>()
+                .Value;
+            return ConnectionMultiplexer.Connect(redisSettings.Connection);
+        });
         infrastructures.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
         infrastructures.AddScoped<IUserRepository, UserRepo>();
         infrastructures.AddScoped<IPasswordHasher, BCryptPasswordHasher>();
         infrastructures.AddScoped<IRefreshTokens, CryptoRefreshTokenGenerator>();
         infrastructures.AddScoped<IRefreshTokenStore, RedisRefreshTokenStore>();
+
         infrastructures.AddDbContext<AppDbContext>(options =>
         {
-            options
-                .UseNpgsql(
-                    configuration.GetConnectionString("DefaultConnection")).UseSnakeCaseNamingConvention();
+            options.UseNpgsql(
+                configuration.GetConnectionString("DefaultConnection"))
+                .UseSnakeCaseNamingConvention();
         });
+
         return infrastructures;
     }
 }
